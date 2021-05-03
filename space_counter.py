fae184f46da4b6682a83799d4b640f1c35a5dccf
erikblank <erikblank97@web.de>
2021-05-03 18:52:10 +0200

    add random rects

--- space_counter.py
+++ space_counter.py
@@ -3,6 +3,8 @@
 
 import sys
 from PyQt5 import QtGui, QtWidgets, QtCore
+import random
+import time
 
 
 class SpaceRecorder(QtWidgets.QWidget):
@@ -10,25 +12,40 @@ class SpaceRecorder(QtWidgets.QWidget):
 
     Every time the 'space' key is pressed, a visual indicator is toggled, too.
     """
+    black = QtGui.QColor(0, 0, 0)
+    white = QtGui.QColor(255, 255, 255)
+    width = 1200
+    height = 800
+    minRectWidth = 40
+    maxRectWidth = 200
+    rectAppeared = False
 
-    def __init__(self):
+    def __init__(self, isDarkmode):
         super().__init__()
+        self.isDarkmode = isDarkmode
         self.counter = 0
         self.initUI()
+        self.color = self.white
 
     def initUI(self):
         # set the text property of the widget we are inheriting
-        self.text = "Please press 'space' repeatedly."
-        self.setGeometry(300, 300, 280, 170)
+        self.text = "Please press 'space' if a new rectangle appears"
+        self.setGeometry(100, 100, self.width, self.height)
         self.setWindowTitle('ClickRecorder')
         # widget should accept focus by click and tab key
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        if self.isDarkmode:
+            self.setStyleSheet('background-color: black')
+        else:
+            self.setStyleSheet('background-color: white')
         self.show()
 
     def keyPressEvent(self, ev):
         if ev.key() == QtCore.Qt.Key_Space:
-            self.counter += 1
-            self.update()
+            if self.rectAppeared:
+                self.counter += 1
+                self.rectAppeared = False
+                self.update()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
@@ -38,26 +55,48 @@ class SpaceRecorder(QtWidgets.QWidget):
         qp.end()
 
     def drawText(self, event, qp):
-        qp.setPen(QtGui.QColor(168, 34, 3))
+        if self.isDarkmode:
+            qp.setPen(self.white)
+        else:
+            qp.setPen(self.black)
         qp.setFont(QtGui.QFont('Decorative', 32))
         if self.counter > 0:
             self.text = str(self.counter)
-        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)
+        qp.drawText(event.rect(), QtCore.Qt.AlignBottom, self.text)
 
     def drawRect(self, event, qp):
-        if (self.counter % 2) == 0:
-            rect = QtCore.QRect(10, 10, 80, 80)
-            qp.setBrush(QtGui.QColor(34, 34, 200))
+        qp.setBrush(self.black)
+        qp.drawRect(QtCore.QRect(10, 10, 10, 10))
+        rect = self.__getRandomRect()
+        if self.isDarkmode:
+            qp.setBrush(self.white)
         else:
-            rect = QtCore.QRect(100, 10, 80, 80)
-            qp.setBrush(QtGui.QColor(200, 34, 34))
-        qp.drawRoundedRect(rect, 10.0, 10.0)
+            qp.setBrush(self.black)
+        self.rectAppeared = True
+        qp.drawRect(rect)
+
+    def __getRandomRect(self):
+        xPos = random.randint(0, self.width - self.maxRectWidth)
+        yPos = random.randint(0, self.height - self.maxRectWidth)
+        height = random.randint(self.minRectWidth, self.maxRectWidth)
+        return QtCore.QRect(xPos, yPos, height, height)
 
 
 def main():
+    isDarkmode = False
+    if len(sys.argv) == 2:
+        if sys.argv[1] in ('True', 'False'):
+            if sys.argv[1] == 'True':
+                isDarkmode = True
+        else:
+            print("Argument has to be 'True' or 'False'")
+            sys.exit()
+    else:
+        print("Set second argument to 'True' to start with dark mode or 'False' to start with light mode")
+        sys.exit()
     app = QtWidgets.QApplication(sys.argv)
     # variable is never used, class automatically registers itself for Qt main loop:
-    space = SpaceRecorder()
+    space = SpaceRecorder(isDarkmode)
     sys.exit(app.exec_())
 
 
